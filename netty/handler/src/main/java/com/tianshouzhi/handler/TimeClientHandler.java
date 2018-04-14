e5ad27817f4343f7469483c013411e5ea953eaa1
“tianshouzhi” <tianshouzhi@126.com>
2018-04-14 23:16:33 +0800

    add codec demo

---
+++ netty/handler/src/main/java/com/tianshouzhi/handler/TimeClientHandler.java
@@ -0,0 +1,30 @@
+package com.tianshouzhi.handler;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+/**
+ * Created by tianshouzhi on 2018/3/9.
+ */
+public class TimeClientHandler extends ChannelInboundHandlerAdapter {
+
+    private byte[] req=("QUERY TIME ORDER" + System.getProperty("line.separator")).getBytes();
+
+    public void channelActive(ChannelHandlerContext ctx) {
+        ByteBuf message = Unpooled.buffer(req.length);
+        message.writeBytes(req);
+        ctx.writeAndFlush(message);
+    }
+
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        String body = (String) msg;
+        System.out.println("Now is:" + body);
+    }
+
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
